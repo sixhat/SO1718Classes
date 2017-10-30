8578dcdf077c3ac609409d247dc231e85361de5f
David.Rodrigues <david@sixhat.net>
2017-10-30 11:09:07 +0000

    Serialization example

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Remember that classes use Slack for communication
 
 <https://so-ue-1718.slack.com/>
 
+Classes are in room 63 
 
-
- 
+Thursdays: 19h
+Fridays: 21h
---
+++ src/week07/ObjectSerialization.java
@@ -0,0 +1,61 @@
+package week07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class ObjectSerialization implements Serializable {
+	/**
+	 * Generated Serial Number..
+	 */
+	private static final long serialVersionUID = 2455495852041045117L;
+
+	private String name;
+	private int size;
+
+	public ObjectSerialization(String name) {
+		// This is the constructor
+		this.name = name;
+		this.size = this.name.length();
+	}
+
+	@Override
+	public String toString() {
+		return name + " : " + size;
+	}
+
+	public static void main(String[] args) {
+		ObjectSerialization ob = new ObjectSerialization("David Sousa-Rodrigues");
+		System.out.println(String.format("%1$13s = %2$20s", "Before Saving", ob));
+
+		try {
+
+			// let's write the object to file
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("ClassData.dat"));
+			out.writeObject(ob);
+			out.close();
+
+			// Let's read the Object from file
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("ClassData.dat"));
+			ObjectSerialization newObject = (ObjectSerialization) in.readObject();
+			System.out.println(String.format("%1$13s = %2$20s", "From File", newObject));
+			in.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// This Exception occurs when reading from the serialized file if the JVM can't
+			// find a corresponding class.
+			e.printStackTrace();
+		}
+
+	}
+}
