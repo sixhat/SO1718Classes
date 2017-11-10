64d6a47ae2c18337b653cce27eaed3bdfbad7931
David.Rodrigues <david@sixhat.net>
2017-11-10 17:43:58 +0000

    Thread Example

---
+++ src/week8/RunningThreads.java
@@ -0,0 +1,43 @@
+package week8;
+
+/**
+ * Example of Threads doing their thing...
+ * @author "David Sousa-Rodrigues"
+ *
+ */
+public class RunningThreads {
+	public RunningThreads() {
+
+	}
+
+	/**
+	 * Launch a new thread
+	 * @param val
+	 */
+	private void launch(final int val) {
+
+		Thread th = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 20; i=i+val) {
+					System.out.println(Thread.currentThread().getName()+" "+val+" "+i);
+					try {
+						Thread.sleep((long) (Math.random()*300));
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		th.start();
+
+	}
+
+	public static void main(String[] args) {
+		RunningThreads rt = new RunningThreads();
+		for (int i = 1; i < 5; i++) {
+			rt.launch(i);
+		}
+	}
+}
